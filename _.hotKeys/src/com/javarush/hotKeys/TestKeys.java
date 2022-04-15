578c27f460ed5dc0f8524f1e6b2e4aee4c3968e1
”Anton <javaClass45@gmail.com>
2022-04-15 16:13:35 +0300

    наполняем git

--- _.hotKeys/src/com/javarush/hotKeys/TestKeys.java
+++ _.hotKeys/src/com/javarush/hotKeys/TestKeys.java
@@ -3,6 +3,15 @@ package com.javarush.hotKeys;
 public class TestKeys {
     public static void main(String[] args) {
 
+        String s = "1166628";
+
+        Integer i = Integer.parseInt(s);
+
+        System.out.println(i);
+
+        int k = 18;
+        System.out.println(k);
+
 
     }
 }
