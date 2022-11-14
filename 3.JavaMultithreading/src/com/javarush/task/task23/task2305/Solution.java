86b593fcb893ad5a7dd717352cdac3e0fefc48a2
Anton <javaClass45@gmail.com>
2022-11-14 13:33:46 +0200

    solved task2305

--- 3.JavaMultithreading/src/com/javarush/task/task23/task2305/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2305/Solution.java
@@ -11,8 +11,16 @@ public class Solution {
     }
 
     public static Solution[] getTwoSolutions() {
+        Solution[] example = new Solution[2];
 
-        return null;
+        example[0] = new Solution();
+        example[1] = new Solution();
+        example[0].innerClasses[0] = example[0].new InnerClass();
+        example[0].innerClasses[1] = example[0].new InnerClass();
+        example[1].innerClasses[0] = example[1].new InnerClass();
+        example[1].innerClasses[1] = example[1].new InnerClass();
+
+        return example;
     }
 
     public static void main(String[] args) {
