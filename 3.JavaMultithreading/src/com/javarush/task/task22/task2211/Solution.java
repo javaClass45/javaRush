9dad74dcbff7976ea10de9e8c458b167db0c4edd
Anton <javaClass45@gmail.com>
2022-11-20 19:09:51 +0200

    solved task2211

--- 3.JavaMultithreading/src/com/javarush/task/task22/task2211/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2211/Solution.java
@@ -1,7 +1,9 @@
 package com.javarush.task.task22.task2211;
 
 import java.io.*;
+import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 
 /* 
 Смена кодировки
@@ -9,5 +11,26 @@ import java.nio.charset.StandardCharsets;
 
 public class Solution {
     public static void main(String[] args) throws IOException {
+//        System.out.println("args = " + args[0]);
+//        System.out.println("args = " + args[1]);
+        Charset utf8 = Charset.forName("UTF-8");
+        Charset windows1251 = Charset.forName("Windows-1251");
+
+        InputStream inStream = new FileInputStream(args[0]);
+        OutputStream outStream = new FileOutputStream(args[1]);
+
+
+        byte[] buffer = new byte[inStream.available()];
+        while (inStream.available() > 0)
+        {
+            inStream.read(buffer); //читаем один байт из потока для чтения
+            String s = new String(buffer,windows1251);
+            buffer = s.getBytes(utf8);
+            outStream.write(buffer); //записываем прочитанный байт в другой поток.
+        }
+
+        inStream.close(); //закрываем потоки
+        outStream.close();
+
     }
 }
