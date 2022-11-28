a362ffa293fe41c2c3d0c3d841ba82b8d62885f5
Anton <javaClass45@gmail.com>
2022-11-28 21:45:56 +0200

    Арканоид task2413: класс Brick и Ball доработал

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Ball.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Ball.java
@@ -1,6 +1,21 @@
 package com.javarush.task.task24.task2413;
 
 public class Ball extends BaseObject{
+
+    private double speed;
+    private double direction;
+    private double dx;
+    private double dy;
+    private boolean isFrozen;
+
+
+    public Ball(double x, double y, double speed, double direction) {
+        super(x, y, 1);
+        this.speed = speed;
+        this.direction = direction;
+        this.isFrozen = true;
+    }
+
     public Ball(double x, double y, double radius) {
         super(x, y, radius);
     }
@@ -13,5 +28,21 @@ public class Ball extends BaseObject{
     @Override
     public void move() {
 
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public double getDirection() {
+        return direction;
+    }
+
+    public double getDx() {
+        return dx;
+    }
+
+    public double getDy() {
+        return dy;
     }
 }
--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Brick.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Brick.java
@@ -1,6 +1,14 @@
 package com.javarush.task.task24.task2413;
 
 public class Brick extends BaseObject{
+
+
+
+    public Brick(double x, double y) {
+        super(x, y, 3);
+    }
+
+
     public Brick(double x, double y, double radius) {
         super(x, y, radius);
     }
