95b9895b1185ebcdc48537dd4c775b94ddfb206f
Anton <javaClass45@gmail.com>
2022-11-30 18:57:29 +0200

    Арканоид task2413: Stand учили двигаться

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Stand.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Stand.java
@@ -1,6 +1,18 @@
 package com.javarush.task.task24.task2413;
 
-public class Stand extends BaseObject{
+public class Stand extends BaseObject {
+    private double speed;
+    private double direction;
+
+
+    public Stand(double x, double y) {
+        super(x, y, 3);
+        speed = 1;
+        direction = 0;
+    }
+
+
+
     public Stand(double x, double y, double radius) {
         super(x, y, radius);
     }
@@ -12,6 +24,28 @@ public class Stand extends BaseObject{
 
     @Override
     public void move() {
+        double dx = speed * direction;
+        x = x + dx;
 
+//        checkBorders(radius, Arkanoid.game.getWidth() - radius + 1, 1, Arkanoid.game.getHeight() + 1);
+    }
+
+
+    void moveLeft() {
+        direction = -1;
+    }
+
+
+    void moveRight() {
+        direction = 1;
+    }
+
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public double getDirection() {
+        return direction;
     }
 }
