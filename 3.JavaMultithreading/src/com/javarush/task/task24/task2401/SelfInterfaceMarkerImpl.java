b0c64037fbdb0a0493b6765dc5938de39f9fa945
Anton <javaClass45@gmail.com>
2022-12-04 17:44:02 +0200

    added task2413+2401+2402

---
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2401/SelfInterfaceMarkerImpl.java
@@ -0,0 +1,15 @@
+package com.javarush.task.task24.task2401;
+
+public class SelfInterfaceMarkerImpl implements SelfInterfaceMarker {
+
+    public void methodOne() {
+        System.out.println("methodOne");
+    }
+
+    public void methodTwo() {
+        System.out.println("methodTwo");
+    }
+
+
+
+}
