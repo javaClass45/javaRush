0ca77c6b825ce7e55f8a53c0cf33cace8de96107
Anton <javaClass45@gmail.com>
2023-02-03 19:47:06 +0200

    task2909-Рефакторинг(4)-solved

--- 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/University.java
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/University.java
@@ -1,10 +1,22 @@
 package com.javarush.task.task29.task2909.human;
 
-public class University extends Student {
+import java.util.*;
+
+public class University{
+
+    private String name;
+    private int age;
+    private List<Student> students = new ArrayList<>();
 
     public University(String name, int age) {
-        super(name, age, 0);
+        this.name = name;
+        this.age = age;
     }
+
+
+//    public University(String name, int age) {
+//       super(name, age, 0);
+//    }
 
     public Student getStudentWithAverageGrade() {
         //TODO:
@@ -18,5 +30,29 @@ public class University extends Student {
 
     public void getStudentWithMinAverageGradeAndExpel() {
         //TODO:
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(List<Student> students) {
+        this.students = students;
     }
 }
