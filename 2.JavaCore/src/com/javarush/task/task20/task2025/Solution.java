9ea3d0c1656134df5965ec2c29c2eb3827aacb71
Anton <javaClass45@gmail.com>
2023-02-16 21:10:13 +0200

    soft - good worked, but validator disapproves %)

--- 2.JavaCore/src/com/javarush/task/task20/task2025/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task20/task2025/Solution.java
@@ -5,13 +5,17 @@ import java.util.*;
 /* 
 Алгоритмы-числа
 */
+// 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 153, 370, 371, 407, 1634, 8208, 9474, 54748, 92727, 93084, 548834
 // Not solved
+//Метод getNumbers должен возвращать массив чисел удовлетворяющих условию задачи
+//В массиве возвращаемом методом getNumbers не хватает элементов или есть лишние.
+
 public class Solution {
 
     public static long[] getNumbers(long N) {
         long[] result = null;
         TreeSet<Long> numbers = new TreeSet<>();
-
+        numbers.add(0L);
         //Array of degrees
         final int SIZE = 12;
         long[][] powLst = new long[SIZE][SIZE];
@@ -22,7 +26,7 @@ public class Solution {
 
         label:
         for (long i = 1; i < N; i++) {
-            // 1. Reject numbers whose sum of digits is the same (reduce to 200,000 thousand variants or less)
+            // 1. Reject numbers whose sum of digits is the same (reduce to 200,000  variants or less)
             // 2. Get the slicing of numbers in the form of an array
             long x = i;
             int lenCount = 0;
@@ -82,28 +86,29 @@ public class Solution {
             //6. Add a result
             if (summa1 == summa2 && summa1 < N)
                 numbers.add(summa1);
-        }
+        }//   label:
         result = new long[numbers.size()];
 
         int count = 0;
         for (Long l : numbers) {
-            result[count] = l;
-            count++;
+            result[count++] = l;
         }
         return result;
     }
 
     public static void main(String[] args) {
         long a = System.currentTimeMillis();
-        System.out.println(Arrays.toString(getNumbers(1000)));
+        System.out.println(Arrays.toString(getNumbers(1000L)));
         long b = System.currentTimeMillis();
-        System.out.println("memory " + (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / (8 * 1024));
+        System.out.println("memory " + (Runtime.getRuntime().totalMemory()
+                - Runtime.getRuntime().freeMemory()) / (8 * 1024));
         System.out.println("time = " + (b - a) / 1000);
 
         a = System.currentTimeMillis();
-        System.out.println(Arrays.toString(getNumbers(1000000)));
+        System.out.println(Arrays.toString(getNumbers(1_000_000L)));
         b = System.currentTimeMillis();
-        System.out.println("memory " + (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / (8 * 1024));
+        System.out.println("memory " + (Runtime.getRuntime().totalMemory()
+                - Runtime.getRuntime().freeMemory()) / (8 * 1024));
         System.out.println("time = " + (b - a) / 1000);
     }
 }
