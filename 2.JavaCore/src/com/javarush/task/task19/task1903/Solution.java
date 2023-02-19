170c1f3bc8b3683dd9d701eb8b3860f21ec8ae6e
Anton <javaClass45@gmail.com>
2023-02-19 22:38:49 +0200

    task1903-solved

--- 2.JavaCore/src/com/javarush/task/task19/task1903/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1903/Solution.java
@@ -9,12 +9,50 @@ import java.util.Map;
 
 public class Solution {
     public static Map<String, String> countries = new HashMap<String, String>();
-
+    static {
+        countries.put("UA", "Ukraine");
+        countries.put("RU", "Russia");
+        countries.put("CA", "Canada");
+    }
     public static void main(String[] args) {
 
     }
 
-    public static class IncomeDataAdapter {
+    public static class IncomeDataAdapter implements Customer, Contact {
+        private IncomeData data;
+
+
+        public IncomeDataAdapter(IncomeData data) {
+            this.data = data;
+        }
+
+        @Override
+        public String getName() {
+            return data.getContactLastName() + ", " + data.getContactFirstName();
+        }
+
+        @Override
+        public String getCompanyName() {
+            return data.getCompany();
+        }
+
+        @Override
+        public String getCountryName() {
+            return countries.get(data.getCountryCode());
+        }
+
+        @Override
+        public String getPhoneNumber() {
+            String part = String.format("%010d", data.getPhoneNumber());
+            String telPart1 = "+" + data.getCountryPhoneCode();
+            String telPart2 = part.substring(0, 3);
+            String telPart3 = part.substring(3, 6);
+            String telPart4 = part.substring(6, 8);
+            String telPart5 = part.substring(8, 10);
+            String telFormated = String.format("%s(%s)%s-%s-%s", telPart1, telPart2, telPart3, telPart4, telPart5);
+            return telFormated;
+        }
+
     }
 
 
