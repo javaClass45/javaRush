f905d141a1241a359cf564d405fd4af2430abbdf
Anton <javaClass45@gmail.com>
2023-02-20 15:59:35 +0200

    task1905-solved

--- 2.JavaCore/src/com/javarush/task/task19/task1905/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1905/Solution.java
@@ -10,16 +10,62 @@ import java.util.Map;
 public class Solution {
     public static Map<String, String> countries = new HashMap<String, String>();
 
+    static {
+        countries.put("UA", "Ukraine");
+        countries.put("RU", "Russia");
+        countries.put("CA", "Canada");
+    }
+
     public static void main(String[] args) {
 
     }
 
-    public static class DataAdapter {
-        public DataAdapter(Customer customer, Contact contact) {
-        }
-    }
+    public static class DataAdapter implements RowItem {
+        private Customer customer;
+        private Contact contact;
 
-    public interface RowItem {
+        public DataAdapter(Customer customer, Contact contact) {
+            this.contact = contact;
+            this.customer = customer;
+        }
+
+        @Override
+        public String getCountryCode() {
+            String result = null;
+            for (Map.Entry<String, String> pair : countries.entrySet()) {
+                if (pair.getValue().equals(customer.getCountryName())) {
+                    result = pair.getKey();
+                    break;
+                }
+            }
+            return result;
+        }
+
+
+        @Override
+        public String getCompany() {
+            return customer.getCompanyName();
+        }
+
+        @Override
+        public String getContactFirstName() {
+            return contact.getName().split(", ")[1];
+        }
+
+        @Override
+        public String getContactLastName() {
+            return contact.getName().split(", ")[0];
+        }
+
+        @Override
+        public String getDialString() {
+            String tel = "callto://" + contact.getPhoneNumber().replaceAll("[()-]","");
+            return tel;
+        }
+ 
+    } //DA
+
+    public static interface RowItem {
         String getCountryCode();        //For example: UA
 
         String getCompany();            //For example: JavaRush Ltd.
@@ -31,13 +77,13 @@ public class Solution {
         String getDialString();         //For example: callto://+380501234567
     }
 
-    public interface Customer {
+    public static interface Customer {
         String getCompanyName();        //For example: JavaRush Ltd.
 
         String getCountryName();        //For example: Ukraine
     }
 
-    public interface Contact {
+    public static interface Contact {
         String getName();               //For example: Ivanov, Ivan
 
         String getPhoneNumber();        //For example: +38(050)123-45-67 or +3(805)0123-4567 or +380(50)123-4567 or ...
