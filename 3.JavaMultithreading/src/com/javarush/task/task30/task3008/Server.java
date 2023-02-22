ef558c388c0b0fa5d1da0cdc5905a905708396c1
Anton <javaClass45@gmail.com>
2023-02-22 21:28:08 +0200

    task3008(chat6) - solved

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -1,10 +1,39 @@
 package com.javarush.task.task30.task3008;
 
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
     public static void main(String[] args) {
+        ConsoleHelper.writeMessage("Input server port: ");
+        try (ServerSocket serverSocket = new ServerSocket(ConsoleHelper.readInt())) {
+            ConsoleHelper.writeMessage("Server started...");
+            while (true) {
+                new Handler(serverSocket.accept()).start();
+            }
+        } catch (Exception e) {
+            ConsoleHelper.writeMessage("Something wrong, Server socket closed.");
+        }
+
+
+    }//    main
+
+
+    private static class Handler extends Thread {
+        private Socket socket;
+
+        public Handler(Socket socket) {
+            this.socket = socket;
+        }
+
+        @Override
+        public void run() {
+
+        }
+    }// Handler
 
 
 
-    }
 
-}
+
+}// Server
