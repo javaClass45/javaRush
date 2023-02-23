92ae2810366c9a53db336ed1e42a5769fa85e128
Anton <javaClass45@gmail.com>
2023-02-23 23:07:19 +0200

    task3008(chat18)+BotClient - solved

---
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/BotClient.java
@@ -0,0 +1,29 @@
+package com.javarush.task.task30.task3008.client;
+
+public class BotClient extends Client {
+
+    @Override
+    protected SocketThread getSocketThread() {
+        return new BotSocketThread();
+    }
+
+    @Override
+    protected boolean shouldSendTextFromConsole() {
+        return false;
+    }
+
+    @Override
+    protected String getUserName() {
+        String botName = "date_bot_" + ((int) (Math.random() * 100));
+        return botName;
+    }
+
+    public static void main(String[] args) {
+        new BotClient().run();
+
+    }
+
+    public class BotSocketThread extends SocketThread {
+    }
+}
+
