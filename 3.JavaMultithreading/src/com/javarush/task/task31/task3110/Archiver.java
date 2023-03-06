5ed6b8839480416259d9e007e2fca3c3b57da44b
Anton <javaClass45@gmail.com>
2023-03-06 21:15:56 +0200

    task3110(Archiver9)-solved

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
@@ -1,29 +1,12 @@
 package com.javarush.task.task31.task3110;
 
-import com.javarush.task.task31.task3110.command.ExitCommand;
 import com.javarush.task.task31.task3110.exception.WrongZipFileException;
 
 import java.io.IOException;
-import java.nio.file.Paths;
-import java.util.Scanner;
-
-//  d:/j1/arc.zip
-//  d:/j1/text.txt
 
 public class Archiver {
-    public static void main(String[] args) throws Exception {
-//        Scanner console = new Scanner(System.in);
-//        System.out.println("ввести полный путь архива:");
-//        String line = console.nextLine();
-//        ZipFileManager zipFileManager = new ZipFileManager(Paths.get(line));
-//
-//         console = new Scanner(System.in);
-//        System.out.println("вввести путь к файлу, который будем архивировать:");
-//        String newArc = console.nextLine();
-//        zipFileManager.createZip(Paths.get(newArc));
-//
-//        ExitCommand exitCommand = new ExitCommand();
-//        exitCommand.execute();
+    public static void main(String[] args) throws IOException {
+
         Operation operation = null;
         do {
             try {
@@ -37,6 +20,8 @@ public class Archiver {
 
         } while (operation != Operation.EXIT);
     }
+
+
     public static Operation askOperation() throws IOException {
         ConsoleHelper.writeMessage("");
         ConsoleHelper.writeMessage("Выберите операцию:");
@@ -49,5 +34,4 @@ public class Archiver {
 
         return Operation.values()[ConsoleHelper.readInt()];
     }
-
 }
