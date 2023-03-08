adfe22a8e993e638e77086833af727b2febb9d6f
Anton <javaClass45@gmail.com>
2023-03-08 19:44:56 +0200

    task3110(Archiver13)-solved

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/FileProperties.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/FileProperties.java
@@ -1,16 +1,12 @@
 package com.javarush.task.task31.task3110;
 
 public class FileProperties {
-
     private String name;
     private long size;
     private long compressedSize;
     private int compressionMethod;
 
-    public FileProperties(String name,
-                          long size,
-                          long compressedSize,
-                          int compressionMethod) {
+    public FileProperties(String name, long size, long compressedSize, int compressionMethod) {
         this.name = name;
         this.size = size;
         this.compressedSize = compressedSize;
@@ -38,6 +34,7 @@ public class FileProperties {
         // Вычисляем степень сжатия
         return 100 - ((compressedSize * 100) / size);
     }
+
     @Override
     public String toString() {
         // Строим красивую строку из свойств
@@ -55,5 +52,4 @@ public class FileProperties {
 
         return builder.toString();
     }
-
 }
