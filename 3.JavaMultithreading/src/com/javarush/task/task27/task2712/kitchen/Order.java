5ad1ba5977caf5e767c2d5f0be6a943fbed930da
Anton <javaClass45@gmail.com>
2023-03-10 17:51:41 +0200

    task2712(Ресторан(1))-solved

---
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/kitchen/Order.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task27.task2712.kitchen;
+
+import com.javarush.task.task27.task2712.Tablet;
+
+import java.util.*;
+
+public class Order {
+
+    private final Tablet tablet;
+
+    protected List<Dish> dishes;
+
+
+    public Order(Tablet tablet) {
+        this.tablet = tablet;
+    }
+
+
+}
