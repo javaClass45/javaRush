7fb5f3306085daf0348953091d5dbfbe083bac35
Anton <javaClass45@gmail.com>
2023-03-11 17:35:31 +0200

    task2712(Ресторан(2))-solved

--- 3.JavaMultithreading/src/com/javarush/task/task27/task2712/kitchen/Dish.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/kitchen/Dish.java
@@ -3,10 +3,23 @@ package com.javarush.task.task27.task2712.kitchen;
 import java.util.Arrays;
 
 public enum Dish {
-    FISH, STEAK, SOUP, JUICE, WATER;
+    FISH,
+    STEAK,
+    SOUP,
+    JUICE,
+    WATER;
 
     public static String allDishesToString() {
-        return Arrays.toString(Dish.values()) ;
+        String result = "";
+
+        for (Dish dish : Dish.values()) {
+            if ("".equals(result)) {
+                result += dish.name();
+            } else {
+                result += ", " + dish.name();
+            }
+        }
+        return result;
     }
 
 
