f2a01327db7205087f14e4dc10e662f335c0bb3a
Anton <javaClass45@gmail.com>
2023-03-24 15:24:37 +0200

    task2213(Tetris-13) - solved

--- 4.JavaCollections/src/com/javarush/task/task22/task2213/Field.java
+++ 4.JavaCollections/src/com/javarush/task/task22/task2213/Field.java
@@ -1,35 +1,9 @@
 package com.javarush.task.task22.task2213;
 
 public class Field {
-
     private int width;
     private int height;
     private int[][] matrix;
-
-    public Field(int width, int height) {
-        this.width = width;
-        this.height = height;
-        matrix = new int[height][width];
-    }
-
-
-
-    public void print() {
-
-    }
-
-    public void removeFullLines() {
-
-    }
-
-    public Integer getValue(int x, int y) {
-        return null;
-    }
-
-    public void setValue(int x, int y, int value) {
-
-    }
-
 
     public int getWidth() {
         return width;
@@ -41,5 +15,36 @@ public class Field {
 
     public int[][] getMatrix() {
         return matrix;
+    }
+
+    public Field(int width, int height) {
+        this.height = height;
+        this.width = width;
+        matrix = new int[height][width];
+    }
+
+    void print() {
+
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                if (matrix[i][j] == 0)
+                    System.out.print(".");
+                else
+                    System.out.print("X");
+            }
+            System.out.println();
+        }
+    }
+
+    void removeFullLines() {
+
+    }
+
+    Integer getValue(int x, int y) {
+        return null;
+    }
+
+    void setValue(int x, int y, int value) {
+
     }
 }
