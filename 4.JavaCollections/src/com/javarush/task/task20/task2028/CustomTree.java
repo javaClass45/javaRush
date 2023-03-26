b84ddd70c645cac2d442a055819dfff81152275d
Anton <javaClass45@gmail.com>
2023-03-26 18:29:01 +0300

    task2028-3 = solved

--- 4.JavaCollections/src/com/javarush/task/task20/task2028/CustomTree.java
+++ 4.JavaCollections/src/com/javarush/task/task20/task2028/CustomTree.java
@@ -1,30 +1,9 @@
 package com.javarush.task.task20.task2028;
 
-import java.util.*;
 import java.io.Serializable;
 import java.util.AbstractList;
 import java.util.Collection;
-
-/* 
-Построй дерево(1)
-
-Амиго, похоже ты уже достаточно окреп. Самое время проверить свои навыки в большой задаче!
-Сегодня реализуем свое дерево немного нестандартным способом(на базе AbstractList).
-Вводную информацию можешь получить используя свой любимый поисковик и текст ниже.
-Элементы дерева должны следовать так как показано на картинке:
-
-Для начала сделаем наше дерево потомком класса AbstractList с параметром типа String,
- а также реализуем интерфейсы Cloneable и Serializable.
-
-Реализацию методов get(int index) и size() пока оставь стандартной.
-
-
-Requirements:
-1. Класс CustomTree должен поддерживать интерфейс Cloneable.
-2. Класс CustomTree должен поддерживать интерфейс Serializable.
-3. Класс CustomTree должен быть потомком класса AbstractList<String>.
-
-*/
+import java.util.List;
 
 public class CustomTree extends AbstractList<String> implements Cloneable, Serializable {
     @Override
@@ -65,5 +44,24 @@ public class CustomTree extends AbstractList<String> implements Cloneable, Seria
     @Override
     public boolean addAll(int index, Collection<? extends String> c) {
         throw new UnsupportedOperationException();
+    }
+
+    static class Entry<T> implements Serializable {
+
+        String elementName;
+        boolean availableToAddLeftChildren, availableToAddRightChildren;
+        boolean newLineRootElement;
+        Entry<T> parent, leftChild, rightChild;
+
+        Entry(String name) {
+            elementName = name;
+            newLineRootElement = false;
+            availableToAddLeftChildren = true;
+            availableToAddRightChildren = true;
+        }
+
+        public boolean isAvailableToAddChildren() {
+            return this.availableToAddRightChildren || this.availableToAddLeftChildren;
+        }
     }
 }
