a95c7e76b40ef539dae51eb7eb1c63afa6efe721
Anton <javaClass45@gmail.com>
2023-03-27 13:37:40 +0300

    task3802 - solved

--- 4.JavaCollections/src/com/javarush/task/task38/task3802/VeryComplexClass.java
+++ 4.JavaCollections/src/com/javarush/task/task38/task3802/VeryComplexClass.java
@@ -1,12 +1,16 @@
 package com.javarush.task.task38.task3802;
 
-/* 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+/*
 Проверяемые исключения (checked exception)
 */
 
 public class VeryComplexClass {
     public void veryComplexMethod() throws Exception {
         //напишите тут ваш код
+        FileReader fileReader = new FileReader("unknown.file.txt");
+
     }
 
     public static void main(String[] args) {
