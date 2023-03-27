d6b7c90abed893d2fc83b93fffca4fc9771218b1
Anton <javaClass45@gmail.com>
2023-03-27 15:10:53 +0300

    task3812 - solved

--- 4.JavaCollections/src/com/javarush/task/task38/task3812/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task38/task3812/Solution.java
@@ -14,10 +14,26 @@ public class Solution {
     }
 
     public static boolean printFullyQualifiedNames(Class c) {
+        try {
+            PrepareMyTest prepareMyTest = (PrepareMyTest) c.getAnnotation(PrepareMyTest.class);
+            for (String fullyQualifiedName : prepareMyTest.fullyQualifiedNames()) {
+                System.out.println(fullyQualifiedName);
+            }
+        } catch (Exception e) {
+            return false;
+        }
         return true;
     }
 
     public static boolean printValues(Class c) {
+        try {
+            PrepareMyTest prepareMyTest = (PrepareMyTest) c.getAnnotation(PrepareMyTest.class);
+            for (Class clazz : prepareMyTest.value()) {
+                System.out.println(clazz.getSimpleName());
+            }
+        } catch (Exception e) {
+            return false;
+        }
         return true;
     }
 }
